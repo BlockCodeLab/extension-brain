4c017fbcbc12eb2a04db3ad8cd45a2249fd70323
Yeqin Gong <gon@gyeq.in>
2024-08-08 00:25:54 +0800

    use aisdks component

--- src/blocks.js
+++ src/blocks.js
@@ -1,19 +1,9 @@
-import hmacSHA256 from 'crypto-js/hmac-sha256';
-import Base64 from 'crypto-js/enc-base64';
 import { Text } from '@blockcode/ui';
+import { provideAskSparkFunctionJs } from '@blockcode/aisdks';
 import translations from './l10n.yaml';
 import iconURI from './icon.svg';
 import brainPyURI from './brain.py';
 
-const SPARKAI_HOST = 'spark-api.xf-yun.com';
-const SPARKAI_PATHNAME = '/v1.1/chat';
-const SPARKAI_APP_ID = 'db45f79e';
-const SPARKAI_API_SECRET = 'MWFiNjVmNDA4YjNhODFkZGE0MGQ1YWRj';
-const SPARKAI_API_KEY = '6a3dfe79b9e9ec588ca65bf3b9d9c847';
-const SPARKAI_DOMAIN = 'general';
-const SPARKAI_TEMPERATURE = 0.4; // 0.1 ~ 1
-const SPARKAI_MAX_TOKENS = 200; // 1 token = 1.5 chinese or 0.8 english
-const SPARKAI_TOP_K = 3; // 1 ~ 6
 const DIALOGS_LENGTH = 3;
 
 export default {
@@ -128,11 +118,12 @@ export default {
         },
       },
       vm(block) {
+        this.definitions_['brain_brains'] = `runtime.data['brain_brains'] = {};`;
         let code = '';
         if (this.STATEMENT_PREFIX) {
           code += this.injectId(this.STATEMENT_PREFIX, block);
         }
-        const askQuestion = provideAskQuestionFunctionJs.call(this);
+        const askQuestion = provideAskQuestionFunctionJs.call(this, 3);
         const question = this.valueToCode(block, 'QUESTION', this.ORDER_NONE) || 0;
         code += `await ${askQuestion}(target.id, String(${question}));\n`;
         return code;
@@ -155,20 +146,6 @@ export default {
     },
   ],
   translations,
-};
-
-const getWebSocketUrl = () => {
-  const date = new Date().toGMTString();
-  const apisecret = localStorage.getItem(`brain.connection.apisecret`) || SPARKAI_API_SECRET;
-  const apikey = localStorage.getItem(`brain.connection.apikey`) || SPARKAI_API_KEY;
-
-  const signatureRaw = `host: ${SPARKAI_HOST}\ndate: ${date}\nGET ${SPARKAI_PATHNAME} HTTP/1.1`;
-  const signature = Base64.stringify(hmacSHA256(signatureRaw, apisecret));
-
-  const authorizationRaw = `api_key="${apikey}", algorithm="hmac-sha256", headers="host date request-line", signature="${signature}"`;
-  const authorization = btoa(authorizationRaw);
-
-  return `wss://${SPARKAI_HOST}${SPARKAI_PATHNAME}?authorization=${authorization}&date=${date}&host=${SPARKAI_HOST}`;
 };
 
 function provideAddPromptFunctionJs() {
@@ -215,10 +192,10 @@ function provideGetLanguageFunctionJs() {
 function provideAskQuestionFunctionJs() {
   this.definitions_['brain_brains'] = `runtime.data['brain_brains'] = {};`;
   const getLanguage = provideGetLanguageFunctionJs.call(this);
-  const appid = localStorage.getItem(`brain.connection.appid`) || SPARKAI_APP_ID;
+  const ask = provideAskSparkFunctionJs.call(this);
   return this.provideFunction_('brain_ask_question', [
-    `const ${this.FUNCTION_NAME_PLACEHOLDER_} = (id, content) => new Promise((resolve) => {`,
-    '  if (!content) return resolve();',
+    `const ${this.FUNCTION_NAME_PLACEHOLDER_} = async (id, content) => {`,
+    `  if (!content) return;`,
     `  if (!runtime.data['brain_brains'][id]) {`,
     `    runtime.data['brain_brains'][id] = {};`,
     `  }`,
@@ -229,46 +206,14 @@ function provideAskQuestionFunctionJs() {
     `  if (dialogs.length > ${DIALOGS_LENGTH}) dialogs.shift();`,
     `  dialogs.push({ role: 'user', content: content });`,
     `  runtime.data['brain_brains'][id].message = '';`,
-    `  const ws = new WebSocket(\`${getWebSocketUrl()}\`); `,
-    '  ws.onopen = () => {',
-    '    ws.send(JSON.stringify({',
-    '      header: {',
-    `          app_id: '${appid}', `,
-    `          uid: '${appid}', `,
-    '        },',
-    '        parameter: {',
-    '          chat: {',
-    `            domain: '${SPARKAI_DOMAIN}', `,
-    `            temperature: ${SPARKAI_TEMPERATURE},`,
-    `            max_tokens: ${SPARKAI_MAX_TOKENS},`,
-    `            top_k: ${SPARKAI_TOP_K},`,
-    '          },',
-    '        },',
-    '        payload: {',
-    '          message: {',
-    '            text: [',
-    "              { role: 'system', content: `${prompts.join('；')}。请用${language}回答，每次只回答一句话。` },",
-    `              ...dialogs,`,
-    '            ],',
-    '          },',
-    '        },',
-    '    }));',
-    '  };',
-    `  ws.onerror = (e) => resolve(); `,
-    `  let message = '';`,
-    '  ws.onmessage = async (e) => {',
-    '    if (!runtime.running) return;',
-    '    const data = JSON.parse(e.data);',
-    '    if (data.header.code !== 0) return resolve(); ',
-    `    message += data.payload.choices.text.map((text) => text.content).join(''); `,
-    '    if (data.header.status === 2) {',
-    '      ws.close();',
-    `      runtime.data['brain_brains'][id].message = message.trim();`,
-    `      if (dialogs.length > ${DIALOGS_LENGTH}) dialogs.shift();`,
-    `      dialogs.push({ role: 'assistant', content: message });`,
-    '      resolve();',
-    '    }',
-    '  };',
-    '});',
+    `  const message = await ${ask}([`,
+    "    { role: 'system', content: `${prompts.join('；')}。请用${language}回答，每次只回答一句话。` },",
+    `    ...dialogs,`,
+    `  ]);`,
+    '  if (!message) return;',
+    `  runtime.data['brain_brains'][id].message = message;`,
+    `  if (dialogs.length > ${DIALOGS_LENGTH}) dialogs.shift();`,
+    `  dialogs.push({ role: 'assistant', content: message });`,
+    `};`,
   ]);
 }
